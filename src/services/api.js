e9ae0981deea2702a345db095a3f8cdc78aea906
GabrielReyes3 <gabriel.reyes2003@outlook.es>
2025-04-01 20:34:54 -0600

    logs2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://server1-rate-limit-xrz4.onrender.com/api/auth', // Asegúrate de que la URL sea la correcta
+    baseURL: 'https://server2-no-rate-limit.onrender.com/api/auth', // Asegúrate de que la URL sea la correcta
     headers: { 'Content-Type': 'application/json' }
 });
 
