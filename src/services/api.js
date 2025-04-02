2715c288306f0849823794c38ef0d9f80bf2bf28
GabrielReyes3 <gabriel.reyes2003@outlook.es>
2025-04-01 18:39:43 -0600

    productivo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api/auth', // Asegúrate de que la URL sea la correcta
+    baseURL: 'https://server1-rate-limit-xrz4.onrender.com/api/auth/',
     headers: { 'Content-Type': 'application/json' }
 });
 
